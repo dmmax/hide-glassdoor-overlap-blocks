7cc42082fcf3f5d79c09d733ddbb49769f35c422
Maksim Dimukhametov <maksim.dimukhametov@gmail.com>
2023-07-31 19:42:19 +0200

    Create hide-blocks-on-load.js

---
+++ hide-blocks-on-load.js
@@ -0,0 +1,9 @@
+window.addEventListener(
+  "load", 
+  () => {
+    document.getElementById("HardsellOverlay").remove();
+    var bodyVar = document.body;
+    bodyVar.style.overflow = null;
+    bodyVar.style.position = null;
+  }, 
+  false);
